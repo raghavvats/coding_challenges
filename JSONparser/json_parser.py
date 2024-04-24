a98d9204e2877fb7d4479ad024f0e22f44547ee6
raghavvats <raghav.vats007@gmail.com>
2024-04-24 01:31:06 -0700

    advanced file access function + broken validation

--- JSONparser/json_parser.py
+++ JSONparser/json_parser.py
@@ -1,13 +1,27 @@
 import json
+import os
+import glob
 
-def load_json(filename):
-    try:
-        with open(filename, 'r') as json_file:
-            json_data = json.load(json_file)
-            return json_data
-    except FileNotFoundError:
-        print(f"Error: {filename} does not exist")
+def load_all_json(folder):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    test_folder = os.path.join(base_dir, folder)
+
+    json_files = glob.glob(os.path.join(base_dir, '**/*.json'), recursive=True)
+    
+    test_files = []
+    for json_file in json_files:
+        try:
+            with open(json_file, 'r') as json_file:
+                test_files.append(json.dumps(json_file))
+        except FileNotFoundError:
+            print(f"Error: {str(json_file)} does not exist")
+    return test_files
+
+def validate_json(data):
+    
+    return 0
+
+folder_name = input("test folder name: ")
+all_json_data = load_all_json(folder_name)
 
 
-file_name = input("file name: ")
-json_data = load_json(file_name)
